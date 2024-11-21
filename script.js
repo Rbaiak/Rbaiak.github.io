dcde59608af3f8292ff191efd6002b142c21945c
Rbaiak <99350399+Rbaiak@users.noreply.github.com>
2024-11-21 17:10:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const custo = 5196
+const botao = document.getElementById("meuBotao")
+const resultado = document.getElementById('resultado')
+
+
+
+botao.addEventListener('click', () => {
+    const valorInput = document.getElementById('meuInput').value;
+
+    // Tenta converter o valor de entrada para um número
+    const numero = Number(valorInput);
+
+    // Verifica se o valor convertido é NaN (não é um número)
+    if (isNaN(numero)) {
+        window.alert("Insira apenas números");
+    } else {
+        resultado.textContent = "Você gastou aproximadamente " + (numero * custo) + " Litros d´água";
+    }
+});
+
